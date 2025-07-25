0a81c1dc2e15fa592e86da39dfaed9a20fe33996
KushanRandeepa <kushanrandeepa55@gmail.com>
2025-07-25 19:45:09 +0530

    install flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(){
+         initFlowbite();
+
+  }
+
+  ngOnInit(): void {
+    // initFlowbite();
+
+  }
   title = 'simplerent';
 }
